79a3551c0c101e8611a898141915cd7cb164d370
soyuz43 <kebekad673@proton.me>
2025-05-15 15:27:29 -0500

    Add DTOs for UserProfile and Reservation

---
+++ Models/DTOs/ReservationDTO.cs
@@ -0,0 +1,10 @@
+namespace CreekRiver.Models.DTOs;
+
+public class ReservationDTO
+{
+    public int Id { get; set; }
+    public int CampsiteId { get; set; }
+    public int UserProfileId { get; set; }
+    public DateTime CheckinDate { get; set; }
+    public DateTime CheckoutDate { get; set; }
+}
